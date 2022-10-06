dfa0104fec11cfb0fe0125b85b262fc0c94b4640
Mikołaj <56467077+mkj-dev@users.noreply.github.com>
2022-10-06 16:49:40 +0200

    Delete factorial.js

--- factorial.js
+++
@@ -1,9 +0,0 @@
-function factorialize(num) {
-  let arr = []
-  if (num === 0) return 1
-  for (let i = 1; i <= num; i++) {
-    arr.push(i)
-  }
-  num = arr.reduce((prev, next) => prev * next)
-  return num;
-}
