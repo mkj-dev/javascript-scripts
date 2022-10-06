28f2cd3703d0234817028c1b21d270b3a487414b
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2022-10-06 20:33:50 +0200

    Return an array consisting of the largest number from each provided sub-array

---
+++ basic_algorithm_scripting/largestOfFour.js
@@ -0,0 +1,13 @@
+// Return an array consisting of the largest number from each provided sub-array
+function largestOfFour(arr) {
+    let newArr = []
+    let number = null
+    arr.forEach(array => {
+        number = Math.max(...array)
+        newArr.push(number)
+    })
+    return newArr;
+}
+
+const largestNumberArray = largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+console.log(largestNumberArray)
