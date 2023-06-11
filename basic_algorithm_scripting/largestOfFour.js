2db8fc51350fe031f69d3333714fa7e1063af0a6
Mikołaj <56467077+mkj-dev@users.noreply.github.com>
2023-06-11 11:33:43 +0200

    Add different solution to the largestOfFour.js

--- basic_algorithm_scripting/largestOfFour.js
+++ basic_algorithm_scripting/largestOfFour.js
@@ -9,5 +9,16 @@ function largestOfFour(arr) {
     return newArr;
 }
 
+// function largestOfFour(arr) {
+//   let largestNumArr = [];
+//   let largest = null;
+
+//   for (let i = 0; i < arr.length; i++) {
+//     largest = Math.max(...arr[i])
+//     largestNumArr.push(largest)
+//   }
+//   return largestNumArr;
+// }
+
 const largestNumberArray = largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
 console.log(largestNumberArray)
