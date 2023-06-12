af2d7b5311c196f440ad4ee545ebf82b5fe06703
Mikołaj <56467077+mkj-dev@users.noreply.github.com>
2023-06-12 11:55:25 +0200

    Function that splits an array into groups and returns 2D array

---
+++ basic_algorithm_scripting/chunkArrayInGroups.js
@@ -0,0 +1,13 @@
+// Write a function that splits an array (first argument) into groups the length of size (second argument)
+// and returns them as a two-dimensional array
+function chunkArrayInGroups(arr, size) {
+  let newArr = []
+  for (let i = 0; i < arr.length; i += size) {
+    newArr.push(arr.slice(i, i + size));
+  }
+  return newArr;
+}
+
+console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4));
